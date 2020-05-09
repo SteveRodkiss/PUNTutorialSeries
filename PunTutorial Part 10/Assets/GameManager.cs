30f5e83ac8c2369f416dbedd0b0cfd8c2f278be7
SteveRodkiss <steve.rodkiss@gmail.com>
2020-05-10 10:39:59 +1200

    Part 10- Start uploaded to part 10 folder

---
+++ PunTutorial Part 10/Assets/GameManager.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+using Photon.Pun.UtilityScripts;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+
+    public GameObject redPlayerPrefab;
+    public GameObject bluePlayerPrefab;
+    public GameObject pauseCanvas;
+    public bool paused = false;
+    public Text messageText;
+
+    public static GameManager instance;
+    public static int blueScore = 0;
+    public static int redScore = 0;
+    public Text blueScoreText;
+    public Text redScoreText;
+
+    public static readonly byte RestartGameEventCode = 1; 
+
+
+    private void Start()
+    {
+        //send a message
+        StartCoroutine(DisplayMessage("Fight!!"));
+
+        //set paused state
+        SetPaused();
+
+        //check that we dont have a local instance before we instantiate the prefab
+        if (NetworkPlayerManager.localPlayerInstance == null)
+        {
+            //instantiate the correct player based on the team
+            int team = (int)PhotonNetwork.LocalPlayer.CustomProperties["Team"];
+            Debug.Log($"Team number {team} is being instantiated");
+            //instantiate the blue player if team is 0 and red if it is not
+            if (team == 0)
+            {
+                //get a spawn for the correct team
+                Transform spawn = SpawnManager.instance.GetTeamSpawn(0);
+                PhotonNetwork.Instantiate(bluePlayerPrefab.name, spawn.position, spawn.rotation);
+            }
+            else
+            {
+                //now for the red team
+                Transform spawn = SpawnManager.instance.GetTeamSpawn(1);
+                PhotonNetwork.Instantiate(redPlayerPrefab.name, spawn.position, spawn.rotation);
+            }
+            
+        }
+    }
+
+    //function to display a message
+    IEnumerator DisplayMessage(string message)
+    {
+        messageText.text = message;
+        yield return new WaitForSeconds(2);
+        messageText.text = "";
+    }
+
+
+    private void Awake()
+    {
+        SetScoreText();
+        instance = this;
+    }
+
+    void SetScoreText()
+    {
+        redScoreText.text = redScore.ToString();
+        blueScoreText.text = blueScore.ToString();
+    }
+
+
+    public void Quit()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            paused = !paused;
+            SetPaused();
+        }
+        SetScoreText();
+        //check score and win
+        if (redScore >= 3)
+        {
+            StartCoroutine(DisplayMessage("Red wins"));
+            //if master client send restart event
+            if (PhotonNetwork.IsMasterClient)
+            {
+                StartCoroutine(RestartGame());
+            }
+
+        }
+        if (blueScore >= 3)
+        {
+            StartCoroutine(DisplayMessage("Blue team wins"));
+            if (PhotonNetwork.IsMasterClient)
+            {
+                StartCoroutine(RestartGame());
+            }
+        }
+
+
+    }
+
+    IEnumerator RestartGame()
+    {
+        redScore = 0;
+        blueScore = 0;
+        yield return new WaitForSeconds(2);
+        RaiseEventOptions raiseEventOptions = new RaiseEventOptions { Receivers = ReceiverGroup.All };
+        ExitGames.Client.Photon.SendOptions sendOptions = new ExitGames.Client.Photon.SendOptions { Reliability = true };
+        PhotonNetwork.RaiseEvent(RestartGameEventCode,null,raiseEventOptions, sendOptions);
+    }
+
+
+    void SetPaused()
+    {
+        //set the canvas
+        pauseCanvas.SetActive(paused);
+        //set the cursoro lock
+        Cursor.lockState = paused ? CursorLockMode.None : CursorLockMode.Locked;
+        //set the cursoro visible
+        Cursor.visible = paused;
+    }
+
+    //reload the level when anyone leaves or joins?- That is done in the demo but is it needed?
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        ReloadLevel();
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        ReloadLevel();
+    }
+
+    public void ReloadLevel()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.Log("Reloading Level");
+            PhotonNetwork.LoadLevel(1);
+        }
+    }
+
+
+}
---
+++ PunTutorial Part 10/Assets/Simple First Person Controller/Scripts/FirstPersonController.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using Photon.Pun.UtilityScripts;
+
+[RequireComponent(typeof(CharacterController))]
+public class FirstPersonController : MonoBehaviourPunCallbacks, IPunObservable
+{
+    /// <summary>
+    /// Move the player charactercontroller based on horizontal and vertical axis input
+    /// </summary>
+
+
+    float yVelocity = 0f;
+    [Range(-5f, -25f)]
+    public float gravity = -15f;
+    //the speed of the player movement
+    [Range(5f, 15f)]
+    public float movementSpeed = 10f;
+    //jump speed
+    [Range(5f, 15f)]
+    public float jumpSpeed = 10f;
+
+    //now the camera so we can move it up and down
+    public Transform cameraTransform;
+    float pitch = 0f;
+    [Range(1f, 90f)]
+    public float maxPitch = 85f;
+    [Range(-1f, -90f)]
+    public float minPitch = -85f;
+    [Range(0.5f, 5f)]
+    public float mouseSensitivity = 2f;
+
+    //the charachtercompononet for moving us
+    CharacterController cc;
+
+    private void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        if (!photonView.IsMine)
+        {
+            GetComponentInChildren<Camera>().enabled = false;
+            GetComponentInChildren<AudioListener>().enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (photonView.IsMine)
+        {
+            Look();
+            Move();
+        }
+
+    }
+
+    void Look()
+    {
+        //get the mouse inpuit axis values
+        float xInput = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float yInput = Input.GetAxis("Mouse Y") * mouseSensitivity;
+        //turn the whole object based on the x input
+        transform.Rotate(0, xInput, 0);
+        //now add on y input to pitch, and clamp it
+        pitch -= yInput;
+        pitch = Mathf.Clamp(pitch, minPitch, maxPitch);
+        //create the local rotation value for the camera and set it
+        Quaternion rot = Quaternion.Euler(pitch, 0, 0);
+        cameraTransform.localRotation = rot;
+    }
+
+    void Move()
+    {
+        //needed this co a warning came up when people tried to move while their character controller was inactive
+        if (cc.enabled == false)
+        {
+            return;
+        }
+        //update speed based onn the input
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        input = Vector3.ClampMagnitude(input, 1f);
+        //transofrm it based off the player transform and scale it by movement speed
+        Vector3 move = transform.TransformVector(input) * movementSpeed;
+        //is it on the ground
+        if (cc.isGrounded)
+        {
+            yVelocity = 0;
+            //check for jump here
+            if (Input.GetButtonDown("Jump"))
+            {
+                yVelocity = jumpSpeed;
+            }
+        }
+        //now add the gravity to the yvelocity
+        yVelocity += gravity * Time.deltaTime;
+        move.y = yVelocity;
+        //and finally move
+        cc.Move(move * Time.deltaTime);
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(pitch);
+        }
+        else
+        {
+            pitch = (float)stream.ReceiveNext();
+            Quaternion rot = Quaternion.Euler(pitch, 0, 0);
+            cameraTransform.localRotation = rot;
+        }
+    }
+}
